24600288290300ad570e8b4bccf35bd5fa103ad3
IdianScam <67170191+IdianScam@users.noreply.github.com>
2020-06-23 15:38:30 -0500

    Map Model and Basic Scripts
    Basic Assests such as walls, bridges tunnels, etc.) Basic maze lay out design including movement.

---
+++ Matthew-FINAL PROJECT ID TECH/Assets/C-Script/PlayerMovement.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public int speed = 200;
+    public int jumpForce = 7;
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float mouseInput = Input.GetAxis("Mouse X");
+
+        Vector3 lookhere = new Vector3(0, mouseInput, 0);
+
+        transform.Rotate(lookhere);
+
+        if (Input.GetKey(KeyCode.W))
+        {
+            transform.Translate(Vector3.forward * speed * Time.deltaTime);
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.Translate(Vector3.left * speed * Time.deltaTime);
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            transform.Translate(Vector3.back * speed * Time.deltaTime);
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.Translate(Vector3.right * speed * Time.deltaTime);
+        }
+    }
+}
---
+++ Matthew-FINAL PROJECT ID TECH/Assets/C-Script/ShooterScript.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShooterScript : MonoBehaviour
+{
+    public GameObject bullet;
+    public bool canShoot;
+    public float timeBetweenShots = 1;
+    private float timeUntilNextShot;
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        if (Time.time > timeUntilNextShot)
+        {
+            canShoot = true;
+        }
+        if (Input.GetMouseButtonDown(0) && canShoot)
+        {
+            canShoot = false;
+            timeUntilNextShot = Time.time + timeBetweenShots;
+            Instantiate(bullet, this.transform.position, this.transform.rotation);
+        }
+    }
+}
