2e6e7497a4850ca745d2da098ec2437b6cd2c3b0
BellaBenson <johnbenson31@hotmail.com>
2020-06-24 16:54:04 -0500

    Game over menu done
    Game over menu done and wall spawns it

---
+++ Matthew-FINAL PROJECT ID TECH/Assets/C-Script/KillVolume.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KillVolume : MonoBehaviour
+{
+    
+    public GameObject losePanel;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        losePanel.gameObject.SetActive(false);  
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag ("Player"))
+        {
+            displayLosePanel();
+            Debug.Log("Hello");
+        }
+
+    }
+    public void displayLosePanel()
+    {
+        losePanel.gameObject.SetActive(true);
+    } 
+}
+
+
