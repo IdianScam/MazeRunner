67d253c6d7cc50e2ca31d74c2b5145b2c3922ccc
BellaBenson <johnbenson31@hotmail.com>
2020-06-23 21:44:50 -0500

    Game over menu (not done)
    i aslo did an enemy

---
+++ Matthew-FINAL PROJECT ID TECH/Assets/C-Script/EnemyFollow.cs
@@ -0,0 +1,27 @@
+﻿
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyFollow : MonoBehaviour
+{
+    public GameObject player;
+    float speed = 4;
+    float radius = 5;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.LookAt(player.transform);
+        if (Vector3.Distance(transform.position, player.transform.position) <= radius)
+        {
+            transform.Translate(Vector3.down * speed * Time.deltaTime);
+        }
+    }
+}
