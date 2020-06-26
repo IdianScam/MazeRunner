c1ab61bf1ba0d87e14139aee74da3bf017902b16
ingensargens <67170142+ingensargens@users.noreply.github.com>
2020-06-26 15:50:25 -0500

    target 2 working
    ^

---
+++ Matthew-FINAL PROJECT ID TECH/Assets/C-Script/TargetSpeedScript.cs
@@ -0,0 +1,38 @@
+﻿using JetBrains.Annotations;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TargetSpeedScript : MonoBehaviour
+{
+    public float timer = 5.0f;
+    public int speed = 20;
+    public GameObject TargetSpeed;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            Destroy(TargetSpeed);
+            Debug.Log("Test");
+            for (timer = 5; timer > 0; timer--) {
+
+
+
+                    speed = 40;
+                                            }
+
+              
+            }
+        }
+    }
+
